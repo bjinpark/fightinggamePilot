6b1f07781015f1217812b34de15516ecc0058c27
Seung Hyun Jin <65264634+seunghjin@users.noreply.github.com>
2024-01-04 21:40:36 -0600

    animated the sprites

--- classes.js
+++ classes.js
@@ -1,6 +1,56 @@
-class Character{
-    constructor({position, velocity, color = 'green', offset}){
+class Sprite {
+    constructor({ position, imageSrc, scale = 1, frames = 1, offset = {x: 0, y: 0}}){
         this.position = position
+        this.width = 50
+        this.height = 150
+        this.image = new Image()
+        this.image.src = imageSrc
+        this.scale = scale
+        this.frames = frames
+        this.currFrame = 0
+        this.framesPassed = 0
+        this.framesHold = 13
+        this.offset = offset
+    }
+
+    draw() {
+        c.drawImage(
+            this.image,
+            this.currFrame * (this.image.width / this.frames),
+            0,
+            this.image.width / this.frames,
+            this.image.height,
+            this.position.x - this.offset.x, this.position.y - this.offset.y, (this.image.width / this.frames) * this.scale, this.image.height * this.scale)
+    }
+
+    animation(){
+        this.framesPassed++
+
+        if(this.framesPassed % this.framesHold === 0) {    
+            if(this.currFrame < this.frames - 1){
+                this.currFrame++            
+            } else {
+                this.currFrame = 0
+            }
+        }
+    }
+
+    update(){
+        this.draw()
+        this.animation()
+    }
+}
+
+
+class Character extends Sprite{
+    constructor({position, velocity, color = 'green', imageSrc, scale = 1, frames = 1, offset = {x: 0, y: 0}, sprites}){
+        super({
+            position, 
+            imageSrc, 
+            scale, 
+            frames, 
+            offset,
+        })
         this.velocity = velocity
         this.height = 150
         this.width = 75
@@ -18,8 +68,18 @@ class Character{
         this.color = color
         this.isAttacking
         this.health = 100
+        this.currFrame = 0
+        this.framesPassed = 0
+        this.framesHold = 13
+        this.sprites = sprites
+
+        for(const sprite in this.sprites){
+            sprites[sprite].image = new Image()
+            sprites[sprite].image.src =  sprites[sprite].imageSrc
+        }
     }
 
+    /*
     draw() {
         c.fillStyle = this.color
         c.fillRect(this.position.x, this.position.y, this.width, this.height)
@@ -30,9 +90,11 @@ class Character{
             c.fillRect(this.hitbox.position.x, this.hitbox.position.y, this.hitbox.width, this.hitbox.height)
         }
     }
+*/
 
     update() {
         this.draw()
+        this.animation()
         this.hitbox.position.x = this.position.x + this.hitbox.offset.x
         this.hitbox.position.y = this.position.y
         this.position.y += this.velocity.y
@@ -50,29 +112,38 @@ class Character{
         // }
     }
 
+    switchS(sprite){
+        switch(sprite){
+            case 'idle':
+                if(this.image !== this.sprites.idle.image) {
+                    this.image = this.sprites.idle.image
+                    this.frames = this.sprites.idle.frames                    
+                }
+                break
+            case 'run':
+                if(this.image !== this.sprites.run.image){
+                    this.image = this.sprites.run.image
+                    this.frames = this.sprites.run.frames
+                }
+                break
+            case 'jump':
+                if(this.image !== this.sprites.jump.image){
+                    this.image = this.sprites.jump.image
+                    this.frames = this.sprites.jump.frames                    
+                }
+                break
+        }
+    }
+
     theattack(){
         this.isAttacking = true
         setTimeout(() => {
             this.isAttacking = false
         }, 100)
     }
+
+
 }
 
 
-class Sprite {
-    constructor({ position, imageSrc }){
-        this.position = position
-        this.width = 50
-        this.height = 150
-        this.image = new Image()
-        this.image.src = imageSrc
-    }
 
-    draw() {
-        c.drawImage(this.image, this.position.x, this.position.y)
-    }
-
-    update(){
-        this.draw()
-    }
-}
